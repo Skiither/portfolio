ad1a52a3e826d5bc9c663eac8bc0c083682f65df
Fernando Neves <fernandodn3006@gmail.com>
2025-08-27 12:34:24 +0100

    construindo a estrutura e seção "sobre mim "

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from "../../components/Titulo";
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/Skiither.png" />
+    <Titulo>Fernando Neves</Titulo>
+  </aside>
+);
+
+export default Sidebar;
